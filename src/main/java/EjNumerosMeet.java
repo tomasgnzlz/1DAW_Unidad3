85afbc0daceedeb66b1b6c4263499159f571bbab
tgonati327 <tgonati327@g.educaand.es>
2023-10-17 12:34:29 +0200

    terminar

---
+++ src/main/java/EjNumerosMeet.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tomas
+ */
+public class EjNumerosMeet {
+    public static void main(String[] args) {
+        //Solicitar al usuario un número concreto de números que debe introducir. 
+        //El programa pedirá al usuario esa cantidad de números, uno a uno, 
+        //y dirá si el número introducido es par, impar. 
+        //Al terminar debe calcular la media aritmética.
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Cuanto num quieres mter");
+        int num = teclado.nextInt();
+        int total =0;
+        int numeros = 0;
+        for (int i = 0; i < num; i++) {
+            System.out.println("Introduce el " + i + " num");
+            numeros = teclado.nextInt();
+            total += numeros;
+        }
+    }
+    
+}
