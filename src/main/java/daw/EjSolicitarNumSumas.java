b600c931e4bdd1a4b0e779ddf8e48c715c65a34d
tgonati327 <tgonati327@g.educaand.es>
2023-10-17 12:25:06 +0200

    listo

--- src/main/java/daw/EjSolicitarNumSumas.java
+++ src/main/java/daw/EjSolicitarNumSumas.java
@@ -4,10 +4,28 @@
  */
 package daw;
 
+import com.sun.source.tree.WhileLoopTree;
+import java.util.Scanner;
+
 /**
  *
  * @author tomas
  */
 public class EjSolicitarNumSumas {
-    
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        int numeroTotal=0;
+        
+        do {            
+            System.out.println("Introduce un número");
+            numero = teclado.nextInt();
+            numeroTotal += numero;
+            
+        } while (numero !=0);
+        
+        System.out.println("Total: " + numeroTotal);
+    }
 }
